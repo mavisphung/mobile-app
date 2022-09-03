d715904802c7fc6ceaf4e87ecc72811ce20c6909
Huy Phung <huypc2410@gmail.com>
2022-09-03 11:06:09 +0700

    [MOD] Update api to call at home screen

--- lib/app/models/response.dart
+++ lib/app/models/response.dart
@@ -1,9 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
 
-import 'package:flutter/foundation.dart';
-
-class ResponseModel2<T> {
+class ResponseModel2 {
   bool? success;
   int? status;
   String? message;
